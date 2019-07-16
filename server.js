df73ac4d2c752abaaf7b87d01b4c3c006eb5c99c
dsanderson90 <dsanderson3@cnm.edu>
2019-07-16 15:10:42 -0600

    fixed npm start script and started reviewed callback vs promise based pattern for requests and error handling

--- server.js
+++ server.js
@@ -1,3 +1,54 @@
+// import util from 'util'
+
+// import {exec} from 'child_process';
+import EventEmitter from 'events';
+
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('change', () => {
+  console.log('Something has changed')
+})
+
+setInterval(() => {
+  eventEmitter.emit('change');
+}, 1000);
+
+
+//  Error First Callback Pattern
+// exec('ls /', (err, stdout, stderr) => {
+//   if(err) {
+//     if(err === 408) {
+//       console.log(err, '408')
+//     }
+//     else {
+//       process.exit();
+//     }
+//   }
+//   else {
+
+//   }
+// });
+
+//Promise based Pattern
+
+// const execP = util.promisify(exec);
+
+// const main = async () => {
+//   try {
+//  const { stdout, stderr } = await execP('ls /');
+//  console.log(stdout)
+// }
+//   catch (error) {
+//     process.exit(1)
+//   }
+// }
+// main();
+
+//  What Happens if you need to handle many events?
+// Use an Event Emitter
+
+
+
 //  How to Create a Web Server, natively in Node
 // import http from 'http';
 // const server = http.createServer();
